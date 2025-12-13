483853eac86d087504697c61dd9aa434467091c8
v420v <ibuki420v@gmail.com>
2025-12-13 13:32:00 +0900

    delete: yabai and skhd

--- symlink.sh
+++ symlink.sh
@@ -6,6 +6,3 @@ mkdir -p ~/.config/nvim
 ln -s ~/dotfiles/alacritty/alacritty.toml ~/.config/alacritty
 ln -s ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.skhdrc ~/.skhdrc
-ln -s ~/dotfiles/.yabairc ~/.yabairc
-
