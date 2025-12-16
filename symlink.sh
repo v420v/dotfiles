c5a55792b206c606c5c2477cbb0d3da0bcd5d698
v420v <ibuki420v@gmail.com>
2025-12-16 23:31:45 +0900

    update: alacritty background opacity

--- symlink.sh
+++ symlink.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-mkdir -p ~/.config/alacritty
-mkdir -p ~/.config/nvim
-
-ln -s ~/dotfiles/alacritty/alacritty.toml ~/.config/alacritty
-ln -s ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
+mkdir -p ~/.config/alacritty && ln -s ~/dotfiles/alacritty/alacritty.toml ~/.config/alacritty
+mkdir -p ~/.config/nvim &&ln -s ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
